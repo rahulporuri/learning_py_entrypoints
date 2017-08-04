2d004ccc9a2a148034cbaa380ab058dd118e1d34
Poruri Sai Rahul <rporuri@enthought.com>
2017-08-04 10:02:01 -0500

    ADD : a python script that can read file types

---
+++ pyreaders/pyreader.py
@@ -0,0 +1,12 @@
+formats = ['.csv']
+
+def main(file_type):
+    if file_type in formats:
+        print("Yes, I can read the file")
+    else:
+        print("No, I cannot read the file")
+
+if __name__ == "__main__":
+    import sys
+    file_type = sys.argv[1]
+    main(file_type)
